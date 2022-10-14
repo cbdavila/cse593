bf51047c5fb6fffa9fc39c670d07ab57d4b1cf49
Carlos Davila <cbdavila@asu.edu>
2022-10-14 09:05:59 -0700

    baseline EC and emulated sig gen working

--- CommandAndControl/Global.asax.cs
+++ CommandAndControl/Global.asax.cs
@@ -16,6 +16,7 @@ namespace CommandAndControl
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
---
+++ EquipmentControl/App_Code/Service.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+    public bool executeSCPI(string deviceName, string command)
+    {
+        bool executed = false;
+        // send the command string to remote device
+        openDeviceSocketConnection();
+        executed = true;
+        return executed;
+    }
+    public void openDeviceSocketConnection()
+    {
+        try
+        {
+            TcpClient client = new TcpClient("localhost", 12345);
+            string message = "Hello world!";
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+
+            // Send the message to the connected TcpServer.
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Sent: {0}", message);
+
+            // Receive the server response.
+
+            // Buffer to store the response bytes.
+            data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+            // Explicit close is not necessary since TcpClient.Dispose() will be
+            // called automatically.
+            // stream.Close();
+            // client.Close();
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("ArgumentNullException: {0}", e);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+    }
+}
